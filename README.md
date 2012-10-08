0d7d642792e1ac17c07cb13cd01333cc2c5baaa4
lehitoskin <lehi_toskin@lavabit.com>
2012-10-08 15:19:47 -0700

    Added the correct FTP library calls

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ An FTP client implemented in Lisp (Racket)
 TODO:
 	Take command-line arguments
 	No arguments, give a help screen
+	Create menu
--- rktftp.rkt
+++ rktftp.rkt
@@ -2,14 +2,37 @@
 #lang racket
 ; rktftp.rkt
 ; an ftp client
-; who knew??
+; information found from
+; 	http://docs.racket-lang.org/net/ftp.html
 
 ; nothing to see here, folks!
 
-(require racket/tcp)
+(require net/ftp)
 
-; open socket
-(open-tcp-stream-socket "web.mit.edu" "ftp")
+; establish a connection
+(ftp-establish-connection server
+			  port-no ; usually 21
+			  user
+			  passwd)
 
-; close socket
-(close-port)
+; need to put fun things here!
+; like a menu or search around for a file
+
+; change directory
+(ftp-cd ftp-conn new-dir)
+
+; return a list of files in cwd
+; if path is given, check there
+; (ftp-directory-list ftp-conn [path])
+; (listof (list/c (one-of/c "-" "d" "l")
+;		string?
+;		string?))
+
+; downloads file from the server's current directory
+; and puts it in local-dir, using the same name.
+; only writes at the end of transfer, will overwrite
+; files of the same name
+; (ftp-download-file ftp-conn local-dir file)
+
+; close connection
+(ftp-close-connection ftp-conn)
