ce3f9433ea6d570c5f1861b92f86c90475129bd4
lehitoskin <lehi_toskin@lavabit.com>
2012-10-19 16:55:26 -0700

    Added command-line argument support

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ An FTP client implemented in Lisp (Racket)
 
 TODO:
 
-	Take command-line arguments
-
 	No arguments, give a help screen
 
 	Create menu
+
+	Upload files
--- rktftp.rkt
+++ rktftp.rkt
@@ -5,34 +5,38 @@
 ; information found from
 ; 	http://docs.racket-lang.org/net/ftp.html
 
-; nothing to see here, folks!
-
 (require net/ftp)
 
-; establish a connection
-(ftp-establish-connection server
-			  port-no ; usually 21
-			  user
-			  passwd)
+(define port-no 21)
+; replace these next two in main menu loop
+(define new-dir (string->path "EBOOKS"))
+(define path (string->path "."))
 
-; need to put fun things here!
-; like a menu or search around for a file
+(command-line
+  #:args (server user passwd)
 
-; change directory
-;(ftp-cd ftp-conn new-dir)
+  ; establish a connection
+  (let ((ftp-conn (ftp-establish-connection server
+					    port-no ; usually 21
+					    user
+					    passwd)))
 
-; return a list of files in cwd
-; if path is given, check there
-; (ftp-directory-list ftp-conn [path])
-; (listof (list/c (one-of/c "-" "d" "l")
-;		string?
-;		string?))
+    ; need to put fun things here!
+    ; like a menu or search around for a file
+    ; change directory
+    (ftp-cd ftp-conn new-dir)
 
-; downloads file from the server's current directory
-; and puts it in local-dir, using the same name.
-; only writes at the end of transfer, will overwrite
-; files of the same name
-; (ftp-download-file ftp-conn local-dir file)
+    ; return a list of files in cwd
+    ; if path is given, check there
+    (display (ftp-directory-list ftp-conn path))
+    (newline)
 
-; close connection
-(ftp-close-connection ftp-conn)
+    ; downloads file from the server's current directory
+    ; and puts it in local-dir, using the same name.
+    ; only writes at the end of transfer, will overwrite
+    ; files of the same name
+    ;(display "Downloading the file!\n")
+    ;(ftp-download-file ftp-conn local-dir file)
+    
+    ; close connection
+    (ftp-close-connection ftp-conn)))
